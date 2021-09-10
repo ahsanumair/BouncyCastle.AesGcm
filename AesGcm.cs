7219523848570716d1034bdd79852aa3f3c7833a
Ahsan Umair <ahsan.umair@outlook.com>
2021-09-10 16:30:34 +0100

    Add project files.

---
+++ AesGcm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.IO;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace BouncyCastle.AesGcm
+{
+	public enum AesGcmStreamMode
+	{ 
+		Read = 0,
+		Write = 1
+	}
+
+	public class StreamSettings
+	{
+		public int MacSize = 32;
+		public byte[] Key;
+		public byte[] Nonce;
+	}
+
+	public class AesGcm
+	{
+		private CipherStream writeCipherStream = null;
+		
+		private CipherStream readCipherStream = null;
+		
+		private StreamSettings settings = null;
+		
+		private long streamLength;
+
+		private const string Algorithm = "AES/GCM/NoPadding";
+		public AesGcm(Stream stream, StreamSettings cryptoSettings, AesGcmStreamMode mode)
+		{
+			settings = cryptoSettings;
+			streamLength = 0;
+
+			KeyParameter key = ParameterUtilities.CreateKeyParameter("AES", settings.Key);
+
+			if (mode == AesGcmStreamMode.Write)
+			{
+				IBufferedCipher writeCipher = CipherUtilities.GetCipher(Algorithm);
+				
+				writeCipher.Init(true, new AeadParameters(key, settings.MacSize, settings.Nonce));
+
+				this.writeCipherStream = new CipherStream(stream, null, writeCipher);
+			}
+			else if (mode == AesGcmStreamMode.Read)
+			{
+				IBufferedCipher readCipher = CipherUtilities.GetCipher(Algorithm);
+				
+				readCipher.Init(false, new AeadParameters(key, settings.MacSize, settings.Nonce));
+
+				streamLength = stream.Length;
+
+				readCipherStream = new CipherStream(stream, readCipher, null);
+			}
+		}
+		public void Write(string text)
+		{
+			EncryptBytes(Encoding.UTF8.GetBytes(text));
+		}
+
+		public void WriteLine(string text)
+		{
+			EncryptBytes(Encoding.UTF8.GetBytes(text + "\n"));
+		}
+
+		public string ReadAllText()
+		{
+			if (streamLength > 0)
+			{
+				var length = streamLength - (settings.MacSize / 8);
+				
+				using (BinaryReader reader = new BinaryReader(readCipherStream, Encoding.UTF8))
+                {
+					return Encoding.UTF8.GetString(DecryptBytes(reader, length));
+				}
+			}
+
+			return string.Empty;
+		}
+
+		private void EncryptBytes(byte[] input)
+		{
+			for (int i = 0; i != input.Length / 2; i++)
+			{
+				writeCipherStream.WriteByte(input[i]);
+			}
+			writeCipherStream.Write(input, input.Length / 2, input.Length - input.Length / 2);
+		}
+
+		private byte[] DecryptBytes(BinaryReader reader, long length)
+		{
+			var bytes = new byte[length];
+
+			for (int i = 0; i != length / 2; i++)
+			{
+				bytes[i] = reader.ReadByte();
+			}
+
+			int remaining = bytes.Length - (int)length / 2;
+
+			byte[] extra = reader.ReadBytes(remaining);
+
+			if (extra.Length < remaining)
+			{
+				throw new EndOfStreamException();
+			}
+
+			extra.CopyTo(bytes, length / 2);
+
+			return bytes;
+		}
+
+		public void Dispose()
+		{
+			if (writeCipherStream != null)
+			{
+				if (writeCipherStream.CanSeek)
+				{
+					writeCipherStream.Flush();
+				}
+				
+				writeCipherStream.Dispose();
+				writeCipherStream = null;
+			}
+
+			if (readCipherStream != null)
+			{
+				if (readCipherStream.CanSeek)
+				{
+					readCipherStream.Flush();
+				}
+				
+				readCipherStream.Dispose();
+				readCipherStream = null;
+			}
+		}
+
+		public static string GetCryptedRandom(int size)
+		{
+			using (var cryptoRandom = new RNGCryptoServiceProvider())
+			{
+				var key = new byte[size];
+				cryptoRandom.GetBytes(key);
+				return Convert.ToBase64String(key);
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace BouncyCastle.AesGcm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new StreamSettings
+            {
+                Nonce = Encoding.UTF8.GetBytes("13b40f16a9844b10ac97b8f8a1108bcd"), // this should be same when encrypting and decrypting, use a random nonce each time.
+                Key = Encoding.UTF8.GetBytes("629f221c5369477ea0060ca962e1ce92") // this should be same when encrypting and decrypting, use a random key each time.
+            };
+
+            var path = @"C:\test.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            ////Encrypt content to file
+            var writeStream = fileInfo.Open(FileMode.Create, FileAccess.Write);
+
+            var writeCrypto = new AesGcm(writeStream, settings, AesGcmStreamMode.Write);
+
+            Console.WriteLine("Encrypting Text...");
+
+            writeCrypto.WriteLine("This text needs to be encrypted using AES GCM encryption.");
+            writeCrypto.WriteLine("This text needs to be encrypted using AES GCM encryption.");
+
+            writeCrypto.Dispose(); //must call dispose to release resources
+
+            Console.WriteLine($"Encrypting finished...\n\n");
+
+            //Decrypt content from file
+            var readStream = fileInfo.OpenRead();
+
+            var readCrypto = new AesGcm(readStream, settings, AesGcmStreamMode.Read);
+            
+            Console.WriteLine("Decrypting Text...");
+
+            var decryptedText = readCrypto.ReadAllText();
+
+            readCrypto.Dispose(); //must call dispose to release resources
+
+            Console.WriteLine("Decrytion finished...\n\n");
+
+            Console.WriteLine(decryptedText);
+            
+            Console.ReadKey();
+        }
+    }
+}
